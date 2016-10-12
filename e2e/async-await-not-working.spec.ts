e8abe1571a79882af61598abe90ad2c04899abbb
Christian Crowhurst <ccrowhurst@ramplc.com>
2016-10-12 21:46:44 +0100

    test(e2e): example of asyn/await not working

---
+++ e2e/async-await-not-working.spec.ts
@@ -0,0 +1,22 @@
+import { browser, by, element } from 'protractor';
+
+describe('async/await support', () => {
+    it('not working', async function() : Promise<any> {
+        browser.get('es5/index.html');
+        const rows = element.all(by.repeater('user in $data'));
+
+        // this works because we're NOT using async/await
+        rows.count().then((val: number) => {
+            if (val > 0) {
+                expect(rows.get(0).isPresent()).toBe(true);
+            }
+        });
+        
+        // the following (commented out code) is equivalent to the above, but fails to compile
+        // with the error "operand for 'await' does not have a valid callable 'then' member"
+
+        // if (await rows.count() > 0) {
+        //     expect(rows.get(0).isPresent()).toBe(true);
+        // }
+    });
+});
